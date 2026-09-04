ecc1d3f39b3591498907b9fcf000b0decf0484c1
limjunhee <ski725400@gmail.com>
2026-09-04 11:16:30 +0900

    Day6_3

--- src/main/java/example/practice3/MovieController.java
+++ src/main/java/example/practice3/MovieController.java
@@ -3,28 +3,42 @@ package example.practice3;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //RESTful API
 public class MovieController {
+    // private O
     @Autowired private MovieService movieService;
     // [1] 영화 등록
-    @PostMapping("/movie")
+    @PostMapping("/api/movie")
     public boolean addMovie(@RequestBody MovieDto movieDto){
         return movieService.addMovie(movieDto);
     }
     // [2] 영화 전체 조회
-    @GetMapping("/movie")
+    @GetMapping("/api/movie")
     public List<MovieDto> findAllMovies(){
         return movieService.findAllMovies();
     }
     // [3] 영화 개별 조회
-
+    @GetMapping("/api/movie/detail")
+    public MovieDto findMovie(@RequestParam(name = "movieid") int movieid){
+        return movieService.findMovie(movieid);
+    }
     // [4] 특정 영화 수정
-
+    @PutMapping("/api/movie")
+    public boolean updateMovie( @RequestBody MovieDto movieDto){
+        return movieService.updateMovie(movieDto);
+    }
     // [5] 특정 영화 삭제(영화번호 기준)
+    @DeleteMapping("api/movie/{movieid}")
+    public boolean deleteMovie(@PathVariable(name = "movieid") int movieid){
+        return movieService.deleteMovie(movieid);
+    }
 }
--- src/main/java/example/practice3/MovieService.java
+++ src/main/java/example/practice3/MovieService.java
@@ -2,27 +2,33 @@ package example.practice3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class MovieService {
-    @Autowired private MovieRepository movieRepository;
+    // private X
+    @Autowired MovieRepository movieRepository;
 
     // [1] 영화 등록
-    public boolean addMovie( MovieDto movieDto){
-        // 1 - dto를 엔티티로 변환하기 
+    public boolean addMovie( MovieDto movieDto ){
+        // 1 - dto -> 엔티티로 변환하기 
         MovieEntity movieEntity = movieDto.toEntity();
 
-        // 2 - 변환한 엔티티를 저장하기
+        // 2 - 변환한 엔티티를 저장하기(entity save)
         MovieEntity savedMovieEntity = movieRepository.save(movieEntity);
 
-        // 3 - 엔티티의 id가 1 이상이라면? (엔티티가 존재한다면?)
+        // 3 - 엔티티의 id가 1 이상이라면? ('저장하려는 엔티티'가 존재한다면?)
         if (savedMovieEntity.getMovieId() >= 1) {
             return true;
+        }else{
+            return false;
         }
-        return false;
+        
     }
 
     // [2] 영화 전체 조회
@@ -31,7 +37,7 @@ public class MovieService {
         List<MovieEntity> entities = movieRepository.findAll();
 
         // 2. 엔티티 출력해야 하니까 dto로 변환
-        List<MovieDto> list = new ArrayList<>(); // 빈 리스트 생성
+        List<MovieDto> list = new ArrayList<>(); // 빈 dto 리스트 생성(entity 말고)
 
         // 모든 엔티티를 DTO로 변환하여 새로운 리스트에 저장하기
         for(MovieEntity entity : entities){
@@ -45,8 +51,51 @@ public class MovieService {
         return list;
     }
     // [3] 영화 개별 조회(영화번호 기준)
+    public MovieDto findMovie(int movieid){
+        // 1. findById() - 영화 번호를 기준으로 개별조회 하기
+        Optional<MovieEntity> optional = movieRepository.findById(movieid);
 
+        // 2. 조회 결과 확인
+        if (optional.isPresent()) {
+            // 3. 엔티티 있으면 엔티티를 꺼내기
+            MovieEntity entity = optional.get();
+
+            // 4. 꺼낸거 DTO로 변환해서 반환하기
+            return MovieDto.from(entity);
+        }
+
+        return null; // * 없으면 null이다.
+    }
     // [4] 특정 영화 수정(영화번호 기준)
+    @Transactional
+    public boolean updateMovie( MovieDto movieDto ){
+        // 1. ID 기준으로 수정할 대상 찾기
+        Optional<MovieEntity> optional = movieRepository.findById(movieDto.getMovieId());
 
+        if (optional.isPresent()) {
+            MovieEntity entity = optional.get();
+
+            entity.setDirector(movieDto.getDirector());
+            entity.setTitle(movieDto.getTitle());
+            entity.setReleaseDate(movieDto.getReleaseDate());
+            entity.setRating(movieDto.getRating());
+
+            return true;
+        }
+        return false;
+    }
     // [5] 특정 영화 삭제(영화번호 기준)
+    public boolean deleteMovie(int movieid){
+        // 1. 영화번호로 삭제할 것 탐색
+        Optional<MovieEntity> optional = movieRepository.findById(movieid);
+
+        // 2. 삭제하려는 해당 영화가 존재하면?
+        if (optional.isPresent()) {
+            movieRepository.deleteById(movieid);
+
+            return true;
+        }
+
+        return false;
+    }
 }
